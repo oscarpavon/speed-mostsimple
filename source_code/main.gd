dbc6dff8d7f3659c129f79117dd54c48c4cebe8f
Oscar Pavon <pavon_oscar@outlook.com>
2025-02-19 07:43:01 +0000

    game created

--- source_code/main.gd
+++ source_code/main.gd
@@ -1,18 +1,22 @@
-extends Node3D
+extends Node
 
-@export var car1 : VehicleBody3D
-@export var car2 : VehicleBody3D
-@export var hud : Control
-@export var camera : Camera3D
-
-
+var track1
 func _ready():
-	car1.player = true
-	car2.player = false
-	hud.player = car1
-	var camera_view = car1.get_node_or_null("camera_view")
-	if camera_view == null:
-		return
+	#get_tree().change_scene_to_file("res://assets/GUI/menu.tscn")
+	#get_tree().change_scene_to_file("res://assets/Scenes/track.tscn")
+	load_and_play()
+
+
+func load_and_play():
+	track1 = load("res://assets/Scenes/track.tscn")
+	var track1_instace = track1.instantiate()
+	add_child(track1_instace)
+	var hud = track1_instace.get_node("CanvasLayer/HUD")
+	var lambo = track1_instace.get_node("lambo1")
+	hud.player = lambo
+	lambo.player = true
+	var camera = track1_instace.get_node("Camera3D")
+	var camera_view = lambo.get_node("camera_view")
 	camera.target = camera_view
 
 
